ab82e42bfbbc8cd5512fec9968bd69ab0b740291
Yaser Ahmady <y@yaserahmady.com>
2025-03-18 18:49:47 +0100

    Add @zackad/prettier-plugin-twig instead of djlint

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   singleQuote: false,
-  plugins: ["@prettier/plugin-php"],
+  twigSingleQuote: false,
+  twigOutputEndblockName: true,
+  twigAlwaysBreakObjects: false,
+  twigMultiTags: [],
+  plugins: [
+    "@prettier/plugin-php",
+    "@zackad/prettier-plugin-twig",
+    "prettier-plugin-tailwindcss",
+  ],
   overrides: [
     {
       files: ["*.php"],
