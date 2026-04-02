4e40edb59571889091fe479f23de47302bdcc8a2
marcograsso <grassomarco@live.it>
2026-04-02 10:26:23 +0200

    Hero carousel fix

--- views/components/hero-carousel/hero-carousel.php
+++ views/components/hero-carousel/hero-carousel.php
@@ -8,42 +8,38 @@ use Extended\ACF\Fields\Select;
 use Extended\ACF\Fields\Tab;
 use Extended\ACF\Fields\Text;
 use Extended\ACF\Fields\WYSIWYGEditor;
-use Extended\ACF\Location;
 
-register_extended_field_group([
-    "title" => "Hero Carousel",
-    "location" => [Location::where("page_type", "=", "front_page")],
-    "fields" => [
-        Repeater::make("Slide", "hero_carousel_slides")
-            ->layout("block")
-            ->collapsed("title")
-            ->fields([
-                Tab::make("Contenuto")->placement("left"),
-                Image::make("Logo", "logo")->format("array"),
-                Text::make("Titolo", "title"),
-                Text::make("Sottotitolo", "subtitle"),
-                WYSIWYGEditor::make("Descrizione", "description")
-                    ->toolbar(["bold", "italic"])
-                    ->tabs("all")
-                    ->disableMediaUpload(),
-                Link::make("Link", "link")->format("array"),
-                Tab::make("Immagine")->placement("left"),
-                Image::make("Immagine di sfondo", "background_image")->format(
-                    "array",
-                ),
-                Tab::make("Stile")->placement("left"),
-                ColorPicker::make("Colore testo", "text_color")->default(
-                    "#ffffff",
-                ),
-                Select::make("Larghezza testo", "text_width")
-                    ->choices([
-                        "w-1/3" => "1/3 colonna",
-                        "w-1/2" => "1/2 colonna",
-                    ])
-                    ->default("w-1/2"),
-            ])
-            ->button("Aggiungi slide"),
-    ],
-    "style" => "",
-    "hide_on_screen" => ["the_content"],
-]);
+return [
+    Repeater::make("Slide", "hero_carousel_slides")
+        ->layout("block")
+        ->collapsed("title")
+        ->button("Aggiungi slide")
+        ->fields([
+            Tab::make("Immagine")->placement("left"),
+            Image::make("Immagine di sfondo", "background_image")->format(
+                "array",
+            ),
+            Tab::make("Testi")->placement("left"),
+            Text::make("Titolo", "title"),
+            Text::make("Sottotitolo", "subtitle"),
+
+            Tab::make("Extra")->placement("left"),
+            Link::make("Link", "link")->format("array"),
+            Image::make("Logo superiore", "logo")->format("array"),
+            WYSIWYGEditor::make("Descrizione", "description")
+                ->toolbar(["bold", "italic"])
+                ->tabs("all")
+                ->disableMediaUpload(),
+
+            Tab::make("Stile")->placement("left"),
+            ColorPicker::make("Colore testo", "text_color")
+                ->default("#ffffff")
+                ->palette(["#ffffff", "#1e1e1e", "#bd1822"]),
+            Select::make("Larghezza testo", "text_width")
+                ->choices([
+                    "w-1/3" => "1/3 colonna",
+                    "w-1/2" => "1/2 colonna",
+                ])
+                ->default("w-1/2"),
+        ]),
+];
