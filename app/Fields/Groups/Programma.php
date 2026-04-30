a029111f769c4844980b4eb4f53b28ad25ce891f
marcograsso <grassomarco@live.it>
2026-04-30 17:05:31 +0200

    programma setup

--- app/Fields/Groups/Programma.php
+++ app/Fields/Groups/Programma.php
@@ -2,12 +2,22 @@
 
 namespace App\Fields\Groups;
 
+use Extended\ACF\Fields\Select;
 use Extended\ACF\Location;
 
 register_extended_field_group([
     "title"              => "Programma",
     "location"           => [Location::where("page_template", "=", "page-programma.php")],
-    "fields"             => [],
+    "fields"             => [
+        Select::make("Tipo programma", "tipo_programma")
+            ->choices([
+                "festival" => "Programma Festival",
+                "doc"      => "Programma Doc",
+                "drama"    => "Programma Drama",
+            ])
+            ->default("festival")
+            ->wrapper(["width" => 33]),
+    ],
     "style"              => "",
     "hide_on_screen"     => ["the_content"],
     "acfe_seamless_style" => 1,
--- page-programma.php
+++ page-programma.php
@@ -11,4 +11,18 @@ use Timber\Timber;
 $context         = Timber::context();
 $context["post"] = Timber::get_post();
 
+$tipo = get_field("tipo_programma") ?: "festival";
+$context["tipo"] = $tipo;
+
+if ($tipo === "doc" || $tipo === "drama") {
+    $post_type = $tipo === "doc" ? "contents-doc" : "contents-drama";
+    $context["posts"] = Timber::get_posts([
+        "post_type"      => $post_type,
+        "post_status"    => "publish",
+        "posts_per_page" => -1,
+        "orderby"        => "title",
+        "order"          => "ASC",
+    ]);
+}
+
 Timber::render("templates/page-programma.twig", $context);
